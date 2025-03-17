dac29414a509e4d9af96570e2a55fd3bc751ef76
viniciusbgr <vini25013@gmail.com>
2025-03-17 13:55:14 -0400

    Improved and fixes on logger

--- internal/log/logger.go
+++ internal/log/logger.go
@@ -1,19 +1,121 @@
 package log
 
-import "log"
+import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
 
-func Info(text string, args ...any) {
-	log.Printf(BLUE+"[INFO]"+RESET+" "+text+"\n", args...)
+type Logger struct {
+	log        *log.Logger
+	DebugLevel DebugLevel
 }
 
-func Warn(text string, args ...any) {
-	log.Printf(YELLOW+"[WARN]"+RESET+" "+text+"\n", args...)
+var (
+	logger *Logger
+
+	ErrWriterNil = errors.New("logger: writer is nil")
+)
+
+func New(out io.Writer, debugLevel DebugLevel) error {
+	if out == nil {
+		return ErrWriterNil
+	}
+
+	defaultFlag := log.LstdFlags
+
+	if debugLevel&DebugLevelError == DebugLevelError {
+		defaultFlag |= log.Llongfile
+	} else if debugLevel&DebugLevelWarn == DebugLevelWarn {
+		defaultFlag |= log.Lshortfile
+	}
+
+	logger = &Logger{log.New(out, "", defaultFlag), debugLevel}
+
+	return nil
 }
 
-func Error(text string, args ...any) {
-	log.Printf(RED+"[ERROR]"+RESET+" "+text+"\n", args...)
+func Error(err any) {
+	if logger.DebugLevel&DebugLevelError != DebugLevelError {
+		return
+	}
+
+	out := fmt.Append(nil, err)
+	logger.log.Output(2, ColorRed+"[ERROR] - "+ColorReset+string(out)+"\n")
 }
 
-func Fatal(text string, args ...any) {
-	log.Printf(RED+"[FATAL]"+RESET+" "+text+"\n", args...)
+func Errorf(format string, v ...any) {
+	if logger.DebugLevel&DebugLevelError != DebugLevelError {
+		return
+	}
+
+	out := fmt.Appendf(nil, format, v...)
+	logger.log.Output(2, ColorRed+"[ERROR] - "+ColorReset+string(out)+"\n")
+}
+
+func Warn(v ...any) {
+	if logger.DebugLevel&DebugLevelWarn != DebugLevelWarn {
+		return
+	}
+
+	logger.log.Printf(ColorYellow+"[WARN] - "+ColorReset+"%s\n", v)
+}
+
+func Warnf(format string, v ...any) {
+	if logger.DebugLevel&DebugLevelWarn != DebugLevelWarn {
+		return
+	}
+
+	out := fmt.Appendf(nil, format, v...)
+	logger.log.Output(2, ColorYellow+"[WARN] - "+ColorReset+string(out)+"\n")
+}
+
+func Info(v ...any) {
+	if logger.DebugLevel&DebugLevelInfo != DebugLevelInfo {
+		return
+	}
+
+	logger.log.Printf(ColorGreen+"[INFO] - "+ColorReset+"%s\n", v)
+}
+
+func Infof(format string, v ...any) {
+	if logger.DebugLevel&DebugLevelInfo != DebugLevelInfo {
+		return
+	}
+
+	out := fmt.Appendf(nil, format, v...)
+	logger.log.Output(2, ColorGreen+"[INFO] - "+ColorReset+string(out)+"\n")
+}
+
+func Debug(format string, v ...any) {
+	if logger.DebugLevel&DebugLevelNone == DebugLevelNone {
+		return
+	}
+
+	out := fmt.Appendf(nil, format, v...)
+	logger.log.Output(2, ColorBlue+"[DEBUG] - "+ColorReset+string(out)+"\n")
+}
+
+func Print(v ...any) {
+	out := fmt.Append(nil, v...)
+	logger.log.Output(2, string(out)+"\n")
+}
+
+func Printf(format string, v ...any) {
+	out := fmt.Appendf(nil, format, v...)
+	logger.log.Output(2, string(out)+"\n")
+}
+
+func Fatal(v ...any) {
+	out := fmt.Append(nil, v...)
+	logger.log.Output(2, ColorRed+"[FATAL] - "+string(out)+ColorReset+"\n")
+	os.Exit(1)
+}
+
+func Fatalf(format string, v ...any) {
+	out := fmt.Appendf(nil, format, v...)
+	logger.log.Output(2, ColorRed+"[FATAL] - "+string(out)+ColorReset+"\n")
+	os.Exit(1)
 }
